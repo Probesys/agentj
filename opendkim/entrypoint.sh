3a010d161caaad0ae4f17d948d846b157eaff3e0
Sébastien Poher <sebastien.poher@probesys.com>
2020-09-30 11:50:13 +0200

    opendkim: use vars in conf files

--- opendkim/entrypoint.sh
+++ opendkim/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+sed -i "s/\$MAIL_HOSTNAME/$MAIL_HOSTNAME/g" /etc/opendkim/TrustedHosts
+sed -i "s/\$MAIL_DOMAINNAME/$MAIL_DOMAINNAME/g" /etc/opendkim/SigningTable
+sed -i "s/\$MAIL_DOMAINNAME/$MAIL_DOMAINNAME/g" /etc/opendkim/KeyTable
+
 for domain in $(cat /etc/opendkim/TrustedHosts)
 do
     if [ "$domain" == "localhost" ] || [ "$domain" == "127.0.0.1" ]; then
