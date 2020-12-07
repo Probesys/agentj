6694eeab099bfaf31b0b6161209e8a46a09a46c7
Sébastien Poher <sebastien.poher@probesys.com>
2020-12-07 12:59:42 +0100

    bump to version 1.0.2

--- app/db_init.sh
+++ app/db_init.sh
@@ -5,14 +5,13 @@ SQL_SCRIPT="/var/www/html/agentj/setup/agentj.sql"
 
 # Wait until db server has initialized itself
 sleep 30
-if /usr/bin/mysql -h db -u root -p$DB_ROOT_PASSWORD -e "USE $DB_NAME"
+if /usr/bin/mysql -h db -u root -p$DB_ROOT_PASSWORD -e "USE agentj; SELECT * FROM domain"
 then
-    if [ -f "$SQL_SCRIPT" ]
-    then
-        /usr/bin/mysql -h db -u root -p$DB_ROOT_PASSWORD $DB_NAME < "$SQL_SCRIPT" \
-        && echo -e "AGENTJ: Database successfully initialized"
-        rm "$SQL_SCRIPT"
-    else
-        echo "Database already initialized"
-    fi
+    echo "Database already initialized"
+    exit
+elif [ -f "$SQL_SCRIPT" ]
+then
+    /usr/bin/mysql -h db -u root -p$DB_ROOT_PASSWORD $DB_NAME < "$SQL_SCRIPT" \
+    && echo -e "AGENTJ: Database successfully initialized"
+    rm "$SQL_SCRIPT"
 fi
