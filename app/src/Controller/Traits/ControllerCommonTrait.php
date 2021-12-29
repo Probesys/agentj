d35ef2dddd354892568a0bd4bd8b6c11b8900f5a
christian <christian.tresvaux@probesys.com>
2021-12-29 10:43:56 +0100

    fix csv import emails

--- app/src/Controller/Traits/ControllerCommonTrait.php
+++ app/src/Controller/Traits/ControllerCommonTrait.php
@@ -11,9 +11,6 @@ trait ControllerCommonTrait
     {
         $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
         $text = trim($text, '-');
-        if (function_exists('iconv')) {
-            $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-        }
         $text = strtolower($text);
         $text = preg_replace('#[^-\w]+#', '', $text);
 
