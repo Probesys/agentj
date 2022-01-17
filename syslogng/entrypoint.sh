c57193c133431de8ebbc832f6716ac3d34cc457c
Sébastien Poher <sebastien.poher@probesys.com>
2022-01-17 17:11:05 +0100

    syslogng: prod cron interval

--- syslogng/entrypoint.sh
+++ syslogng/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Installing crontabs"
-echo "* * * * * find /var/log/syslogng/ -daystart -mtime +31 -type f -exec rm {} \;" >> /etc/cron.d/syslogng-rotate
+echo "0 1 * * * find /var/log/syslogng/ -daystart -mtime +31 -type f -exec rm {} \;" >> /etc/cron.d/syslogng-rotate
 crond
 
 exec "$@"
