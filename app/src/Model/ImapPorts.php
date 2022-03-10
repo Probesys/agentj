6645c4012976ec4819f0e1027181adb0a25c4242
Christian TRESVAUX <christian.tresvaux@probesys.com>
2022-03-10 15:15:01 +0100

    fixes #105 add dropdown for domain imap port

---
+++ app/src/Model/ImapPorts.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model;
+
+use App\Util\Enum;
+
+// TODO: singulariser
+class ImapPorts extends Enum
+{
+    public const _143 = '143';
+    public const _465 = '465';
+    public const _993 = '993';
+
+}
