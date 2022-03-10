8afe586dcd9a4ee4a9cf3a7a01bd58f1f8a059f9
Sébastien Poher <sebastien.poher@probesys.com>
2022-03-10 16:19:26 +0100

    opendkim: add key gen script

---
+++ app/docker/conf/opendkim/gendomainkey.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+_MX="$1"
+_DOMAIN="${1#*.}"
+
+echo "$_DOMAIN" >> /etc/opendkim/DomainsList
+echo "$_MX" >> /etc/opendkim/TrustedHosts
+echo "*@$_DOMAIN mail._domainkey.$_DOMAIN" >> /etc/opendkim/SigningTable
+echo "mail._domainkey.$_DOMAIN $_DOMAIN:mail:/etc/opendkim/keys/$_DOMAIN/mail.private" >> /etc/opendkim/KeyTable
+
+if [ -d /etc/opendkim/keys/"$_DOMAIN" ]; then
+    echo "This domain already has DKIM keys. Nothing to do."
+else
+    echo "No DKIM keys exist for this domain. Generating new ones."
+    mkdir -p /etc/opendkim/keys/"$_DOMAIN"
+    cd /etc/opendkim/keys/"$_DOMAIN" || exit
+    opendkim-genkey -s mail -d "$_DOMAIN"
+    chown -R opendkim:opendkim /etc/opendkim/keys
+fi
+
+exit $?
