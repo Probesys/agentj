8e053446b1e092117b4faa611a0bf476e4bb9f2b
Sébastien Poher <sebastien.poher@probesys.com>
2022-03-28 16:52:55 +0200

    dkim: use custom selector

--- app/docker/conf/opendkim/gendomainkey.sh
+++ app/docker/conf/opendkim/gendomainkey.sh
@@ -5,8 +5,8 @@ _MX="$2"
 
 echo "$_DOMAIN" >> /etc/opendkim/DomainsList
 echo "$_MX" >> /etc/opendkim/TrustedHosts
-echo "*@$_DOMAIN mail._domainkey.$_DOMAIN" >> /etc/opendkim/SigningTable
-echo "mail._domainkey.$_DOMAIN $_DOMAIN:mail:/etc/opendkim/keys/$_DOMAIN/mail.private" >> /etc/opendkim/KeyTable
+echo "*@$_DOMAIN agentj._domainkey.$_DOMAIN" >> /etc/opendkim/SigningTable
+echo "agentj._domainkey.$_DOMAIN $_DOMAIN:agentj:/etc/opendkim/keys/$_DOMAIN/mail.private" >> /etc/opendkim/KeyTable
 
 if [ -d /etc/opendkim/keys/"$_DOMAIN" ]; then
     echo "This domain already has DKIM keys. Nothing to do."
@@ -14,7 +14,7 @@ else
     echo "No DKIM keys exist for this domain. Generating new ones."
     mkdir -p /etc/opendkim/keys/"$_DOMAIN"
     cd /etc/opendkim/keys/"$_DOMAIN" || exit
-    opendkim-genkey -s mail -d "$_DOMAIN"
+    opendkim-genkey -s agentj -d "$_DOMAIN"
     sudo chown -R opendkim:opendkim /etc/opendkim
 fi
 
