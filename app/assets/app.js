4fa27399d510eb1126a20ea5539b29a5242a41a6
Christian TRESVAUX <christian.tresvaux@probesys.com>
2022-05-19 14:47:04 +0200

    Squashed commit of the following:
    commit a3e83b97d25c000064531128636ef9b43088cc18
    Author: Christian TRESVAUX <christian.tresvaux@probesys.com>
    Date:   Thu May 19 14:44:36 2022 +0200
    
        fix js for turbo
    
    commit 3317b1ddbdfd2f24f1b741fbca2f27179387abe9
    Author: Christian TRESVAUX <christian.tresvaux@probesys.com>
    Date:   Thu May 19 09:59:50 2022 +0200
    
        enabled turbo drive
    
    commit 52adddcb4501ed693ffa169a167aac6f1cd78d8e
    Author: Christian TRESVAUX <christian.tresvaux@probesys.com>
    Date:   Wed May 18 17:41:15 2022 +0200
    
        integration stimulus + sass loader

--- app/assets/app.js
+++ app/assets/app.js
@@ -6,7 +6,21 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
 
+import './styles/app.css';
+import './styles/scss/styles.scss';
 // start the Stimulus application
 import './bootstrap';
+
+document.addEventListener('turbo:visit', () => {
+  // fade out the old body
+  document.body.classList.add('turbo-loading');
+});
+
+document.addEventListener('turbo:render', () => {
+  // after rendering, we first allow the turbo-loading class to set the low opacity
+  // THEN, one frame later, we remove the turbo-loading class, which allows the fade in
+  requestAnimationFrame(() => {
+    document.body.classList.remove('turbo-loading');
+  });
+});
