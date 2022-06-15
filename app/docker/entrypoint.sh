df9d43ef969179513471defcbb5d6d544b73176c
Sébastien Poher <sebastien.poher@probesys.com>
2022-06-15 17:44:35 +0200

    Merge branch 'bugfix-128-all_domains_dkim' into 140-dkim

--- app/docker/entrypoint.sh
+++ app/docker/entrypoint.sh
@@ -38,7 +38,7 @@ cd /var/www/agentj && sudo -u www-data php8 bin/console doctrine:database:create
 cd /var/www/agentj && sudo -u www-data php8 bin/console doctrine:migration:migrate
 
 echo "Create or update super admin user"
-cd /var/www/agentj && php8 bin/console agentj:create-super-admin $SUPER_ADMIN_USERNAME $SUPER_ADMIN_password
+cd /var/www/agentj && php8 bin/console agentj:create-super-admin $SUPER_ADMIN_USERNAME $SUPER_ADMIN_PASSWORD
 
 # Allow web server user to write Symphony logs
 rm -rf /var/www/agentj/var/cache
