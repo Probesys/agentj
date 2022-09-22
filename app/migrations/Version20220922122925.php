b4a8aac8a30b655500d91c2cc2d86ac1e3aa89f2
Christian TRESVAUX <christian.tresvaux@probesys.com>
2022-09-22 14:46:55 +0200

    update migration file

--- app/migrations/Version20220922122925.php
+++ app/migrations/Version20220922122925.php
@@ -20,34 +20,14 @@ final class Version20220922122925 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE domain DROP FOREIGN KEY FK_A7A91E0B1B8DEA76');
-        $this->addSql('DROP TABLE ajconfiguration');
-        $this->addSql('DROP TABLE captcha');
-        $this->addSql('DROP TABLE ext_log_entries');
-        $this->addSql('DROP TABLE migration_versions');
-        $this->addSql('DROP TABLE users_groups');
         $this->addSql('ALTER TABLE connector DROP FOREIGN KEY FK_148C456E115F0EE5');
         $this->addSql('ALTER TABLE connector ADD CONSTRAINT FK_148C456E115F0EE5 FOREIGN KEY (domain_id) REFERENCES domain (id) ON DELETE CASCADE');
-        $this->addSql('DROP INDEX IDX_A7A91E0B1B8DEA76 ON domain');
-        $this->addSql('ALTER TABLE domain DROP captcha_id');
-        $this->addSql('ALTER TABLE msgs CHANGE originating originating CHAR(1) DEFAULT \' \' NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE ajconfiguration (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, value VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE captcha (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE ext_log_entries (id INT AUTO_INCREMENT NOT NULL, action VARCHAR(8) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, logged_at DATETIME NOT NULL, object_id VARCHAR(64) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, object_class VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, version INT NOT NULL, data LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:array)\', username VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, INDEX log_user_lookup_idx (username), INDEX log_version_lookup_idx (object_id, object_class, version), INDEX log_class_lookup_idx (object_class), INDEX log_date_lookup_idx (logged_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE migration_versions (version VARCHAR(255) CHARACTER SET utf8mb3 NOT NULL COLLATE `utf8mb3_unicode_ci`, PRIMARY KEY(version)) DEFAULT CHARACTER SET utf8mb3 COLLATE `utf8mb3_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE users_groups (user_id INT UNSIGNED NOT NULL, groups_id INT NOT NULL, INDEX IDX_FF8AB7E0F373DCF (groups_id), INDEX IDX_FF8AB7E0A76ED395 (user_id), PRIMARY KEY(user_id, groups_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE users_groups ADD CONSTRAINT FK_FF8AB7E0F373DCF FOREIGN KEY (groups_id) REFERENCES groups (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE users_groups ADD CONSTRAINT FK_FF8AB7E0A76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE connector DROP FOREIGN KEY FK_148C456E115F0EE5');
         $this->addSql('ALTER TABLE connector ADD CONSTRAINT FK_148C456E115F0EE5 FOREIGN KEY (domain_id) REFERENCES domain (id)');
-        $this->addSql('ALTER TABLE domain ADD captcha_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE domain ADD CONSTRAINT FK_A7A91E0B1B8DEA76 FOREIGN KEY (captcha_id) REFERENCES captcha (id)');
-        $this->addSql('CREATE INDEX IDX_A7A91E0B1B8DEA76 ON domain (captcha_id)');
-        $this->addSql('ALTER TABLE msgs CHANGE originating originating CHAR(1) DEFAULT \'\' NOT NULL');
     }
 }
