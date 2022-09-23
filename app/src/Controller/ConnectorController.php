36dd88651d8fffc861c6843f92bbe5a22c50b8d5
Christian TRESVAUX <christian.tresvaux@probesys.com>
2022-09-23 10:40:40 +0200

    addes translation

--- app/src/Controller/ConnectorController.php
+++ app/src/Controller/ConnectorController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/connector')]
 class ConnectorController extends AbstractController {
@@ -24,9 +25,10 @@ class ConnectorController extends AbstractController {
     
 
     #[Route('/delete/{id}', name: 'app_connector_delete', methods: ['GET', 'POST'])]
-    public function delete(Request $request, Connector $connector, ConnectorRepository $connectorRepository): Response {
-        if ($this->isCsrfTokenValid('sync' . $connector->getId(), $request->query->get('_token'))) {
+    public function delete(Request $request, Connector $connector, ConnectorRepository $connectorRepository, TranslatorInterface $translator): Response {        
+        if ($this->isCsrfTokenValid('delete' . $connector->getId(), $request->query->get('_token'))) {
             $connectorRepository->remove($connector, true);
+            $this->addFlash('success', $translator->trans('Message.Flash.deleteSuccesFull'));
         }
 
         return $this->redirectToRoute('domain_edit', ['id' => $connector->getDomain()->getId()], Response::HTTP_SEE_OTHER);
