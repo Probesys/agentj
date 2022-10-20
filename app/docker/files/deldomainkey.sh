403018ad74c31e5c698f7d8f7683ac595bb198b9
Sébastien Poher <sebastien.poher@probesys.com>
2022-10-20 17:49:05 +0200

    fix wrong key path in opendkim scripts

--- app/docker/files/deldomainkey.sh
+++ app/docker/files/deldomainkey.sh
@@ -40,14 +40,14 @@ fi
 
 if [ ! -d /var/db/dkim/keys/"$_DOMAIN" ]; then
     echo "This domain does not have DKIM keys. Nothing to do."
-else                                                                     
-    echo "Removing DKIM keys and config for this domain..."                                                               
-    sed -i "/$_DOMAIN/d" /var/db/dkim/DomainsList                              
-    sed -i "/$_MX/d" /var/db/dkim/TrustedHosts                                                                                  
-    sed -i "/*@$_DOMAIN agentj._domainkey.$_DOMAIN/d" /var/db/dkim/SigningTable                                                 
-    sed -i "/agentj._domainkey.$_DOMAIN $_DOMAIN:agentj:\/etc\/opendkim\/keys\/$_DOMAIN\/agentj.private/d" /var/db/dkim/KeyTable
-    sudo /bin/rm -rf /var/db/dkim/keys/"$_DOMAIN"                   
+else
+    echo "Removing DKIM keys and config for this domain..."
+    sed -i "/$_DOMAIN/d" /var/db/dkim/DomainsList
+    sed -i "/$_MX/d" /var/db/dkim/TrustedHosts
+    sed -i "/*@$_DOMAIN agentj._domainkey.$_DOMAIN/d" /var/db/dkim/SigningTable
+    sed -i "/agentj._domainkey.$_DOMAIN $_DOMAIN:agentj:\/var\/db\/dkim\/keys\/$_DOMAIN\/agentj.private/d" /var/db/dkim/KeyTable
+    sudo /bin/rm -rf /var/db/dkim/keys/"$_DOMAIN"
     sudo /bin/chown -R opendkim:opendkim /var/db/dkim
-fi                                                     
-                                                       
+fi
+
 exit $?
