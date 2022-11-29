64d9b429549dced84716517192669ed11e91aa61
Christian TRESVAUX <christian.tresvaux@probesys.com>
2022-11-29 17:33:39 +0100

    make ldapconnector form element required

--- app/src/Form/LdapConnectorType.php
+++ app/src/Form/LdapConnectorType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\LdapConnector;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,30 +15,39 @@ class LdapConnectorType extends ConnectorType
         parent::buildForm($builder, $options);
         $builder
             ->add('ldapHost', null, [
+                'required' => true,
                 'label' => 'Entities.LdapConnector.fields.ldapHost',
             ])
             ->add('ldapPort', null, [
+                'required' => true,
                 'label' => 'Entities.LdapConnector.fields.ldapPort',
             ])
             ->add('LdapBaseDN', null, [
+                'required' => true,
                 'label' => 'Entities.LdapConnector.fields.LdapBaseDN',
             ])
             ->add('ldapRootDn', null, [
+                'required' => true,
                 'label' => 'Entities.LdapConnector.fields.ldapRootDn',
             ])                
-            ->add('LdapPassword', null, [
+            ->add('LdapPassword', PasswordType::class, [
+                'required' => is_null($builder->getData()->getId()),
                 'label' => 'Entities.LdapConnector.fields.LdapPassword',
             ])
             ->add('ldapLoginField', null, [
+                'required' => true,
                 'label' => 'Entities.LdapConnector.fields.ldapLoginField',
             ])     
             ->add('ldapRealNameField', null, [
+                'required' => true,
                 'label' => 'Entities.LdapConnector.fields.ldapRealNameField',
             ])
             ->add('ldapEmailField', null, [
+                'required' => true,
                 'label' => 'Entities.LdapConnector.fields.ldapEmailField',
             ])
             ->add('ldapGroupField', null, [
+                'required' => true,
                 'label' => 'Entities.LdapConnector.fields.ldapGroupField',
             ])                   
         ;
