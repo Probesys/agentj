c888185a30b2509694d6a6217f4e8d407b10c603
Cyril ZORMAN <cyril.zorman@probesys>
2023-10-03 17:04:59 +0200

    clean postfix et amvis

--- smtp/entrypoint.sh
+++ smtp/entrypoint.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 set -e
-
+IPV4_NETWORK=$(ip route | grep  kernel | awk '{ print $1}')
 # Set mailname
-sed -i "s/\$MAIL_HOSTNAME/$MAIL_HOSTNAME/g" /etc/postfix/main.cf
-sed -i "s/\$IPV4_NETWORK/$IPV4_NETWORK/g" /etc/postfix/main.cf
-sed -i "s/\$IPV4_NETWORK/$IPV4_NETWORK/g" /etc/postfix/master.cf
-echo $MAIL_HOSTNAME > /etc/mailname
+sed -i "s/\$MAIL_HOSTNAME/$MAIL_HOSTNAME/g" /etc/conf/$SMTP_TYPE/postfix/main.cf
+sed -i "s~\$IPV4_NETWORK~$IPV4_NETWORK~g" /etc/conf/$SMTP_TYPE/postfix/main.cf
+sed -i "s~\$IPV4_NETWORK~$IPV4_NETWORK~g" /etc/conf/$SMTP_TYPE/postfix/master.cf
+echo $MAIL_HOSTNAME > /etc/conf/$SMTP_TYPE/mailname
 
-# Configure transport map
-sed -i "s/\$DB_NAME/$DB_NAME/g" /etc/postfix/mysql-transport_map.cf
-sed -i "s/\$DB_USER/$DB_USER/g" /etc/postfix/mysql-transport_map.cf
-sed -i "s/\$DB_PASSWORD/$DB_PASSWORD/g" /etc/postfix/mysql-transport_map.cf
+if [ $SMTP_TYPE != "relay" ] 
+then
+	# Configure transport map
+	sed -i "s/\$DB_NAME/$DB_NAME/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
+	sed -i "s/\$DB_USER/$DB_USER/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
+	sed -i "s/\$DB_PASSWORD/$DB_PASSWORD/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
 
-# Configure recipients map
-sed -i "s/\$DB_NAME/$DB_NAME/g" /etc/postfix/mysql-virtual_recipient_maps.cf
-sed -i "s/\$DB_USER/$DB_USER/g" /etc/postfix/mysql-virtual_recipient_maps.cf
-sed -i "s/\$DB_PASSWORD/$DB_PASSWORD/g" /etc/postfix/mysql-virtual_recipient_maps.cf
+	# Configure recipients map
+	sed -i "s/\$DB_NAME/$DB_NAME/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
+	sed -i "s/\$DB_USER/$DB_USER/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
+	sed -i "s/\$DB_PASSWORD/$DB_PASSWORD/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
 
-# Configure domaines map
-sed -i "s/\$DB_NAME/$DB_NAME/g" /etc/postfix/mysql-virtual_domains.cf
-sed -i "s/\$DB_USER/$DB_USER/g" /etc/postfix/mysql-virtual_domains.cf
-sed -i "s/\$DB_PASSWORD/$DB_PASSWORD/g" /etc/postfix/mysql-virtual_domains.cf
+	# Configure domaines map
+	sed -i "s/\$DB_NAME/$DB_NAME/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
+	sed -i "s/\$DB_USER/$DB_USER/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
+	sed -i "s/\$DB_PASSWORD/$DB_PASSWORD/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
+fi
 
 # Fix file permissions
-find /etc/postfix/ -type f -exec chmod 644 {} \;
+find /etc/conf/$SMTP_TYPE/postfix/ -type f -exec chmod 644 {} \;
 
 exec "$@"
