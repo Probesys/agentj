488bcb6b15783f5907b6577c4331ab4b49fe9faa
Christian TRESVAUX <christian.tresvaux@probesys.com>
2023-11-24 10:46:36 +0100

    Change php8 package name to php81

--- app/docker/entrypoint.sh
+++ app/docker/entrypoint.sh
@@ -35,20 +35,20 @@ sed -i "s|\$OAUTH_AZURE_CLIENT_ID|$OAUTH_AZURE_CLIENT_ID|g" /var/www/agentj/.env
 sed -i "s|\$OAUTH_AZURE_CLIENT_SECRET|$OAUTH_AZURE_CLIENT_SECRET|g" /var/www/agentj/.env
 sed -i "s|\$TRUSTED_PROXIES|$TRUSTED_PROXIES|g" /var/www/agentj/.env
 sed -i "s|\$TZ|$TZ|g" /var/www/agentj/.env
-sed -i 's|memory_limit = 128M|memory_limit = 512M|g' /etc/php8/php.ini
+sed -i 's|memory_limit = 128M|memory_limit = 512M|g' /etc/php81/php.ini
 
 echo "Installing assets"
-cd /var/www/agentj && sudo -u www-data php8 bin/console assets:install
+cd /var/www/agentj && sudo -u www-data php bin/console assets:install
 
 echo "Create database if not exists and update schemas"
-cd /var/www/agentj && sudo -u www-data php8 bin/console doctrine:database:create --if-not-exists
-cd /var/www/agentj && sudo -u www-data php8 bin/console doctrine:migration:migrate
+cd /var/www/agentj && sudo -u www-data php bin/console doctrine:database:create --if-not-exists
+cd /var/www/agentj && sudo -u www-data php bin/console doctrine:migration:migrate
 
 echo "Create or update super admin user"
-cd /var/www/agentj && php8 bin/console agentj:create-super-admin $SUPER_ADMIN_USERNAME $SUPER_ADMIN_PASSWORD
+cd /var/www/agentj && php bin/console agentj:create-super-admin $SUPER_ADMIN_USERNAME $SUPER_ADMIN_PASSWORD
 
 echo "update groups wblist"
-cd /var/www/agentj && php8 bin/console agentj:update-groups-wblist
+cd /var/www/agentj && php bin/console agentj:update-groups-wblist
 
 # Allow web server user to write Symphony logs
 rm -rf /var/www/agentj/var/cache
