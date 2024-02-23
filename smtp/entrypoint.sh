ab70062d5543d69f47bde71cd97bc7d81280af07
Loïs Poujade <lois.poujade@probesys.com>
2024-02-23 16:43:21 +0100

    Use $RELAYHOST in relay smtp

--- smtp/entrypoint.sh
+++ smtp/entrypoint.sh
@@ -23,6 +23,15 @@ then
 	sed -i "s/\$DB_NAME/$DB_NAME/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
 	sed -i "s/\$DB_USER/$DB_USER/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
 	sed -i "s/\$DB_PASSWORD/$DB_PASSWORD/g" /etc/conf/$SMTP_TYPE/postfix/mysql-*.cf
+else
+	if [ -n "$RELAYHOST" ]
+	then
+	    echo "relayhost = $RELAYHOST" >> /etc/postfix/main.cf
+	else
+	    echo "relayhost="  >> /etc/postfix/main.cf
+	fi
+
+	postmap /etc/conf/$SMTP_TYPE/postfix/slow_dest_domains_transport
 fi
 
 # Fix file permissions
