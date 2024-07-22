73855ca07bdd6dfc18c804bbc51ae019b5f82e10
Christian TRESVAUX <christian.tresvaux@probesys.com>
2024-07-22 12:01:05 +0200

    Update entrypoint

--- app/docker/entrypoint.sh
+++ app/docker/entrypoint.sh
@@ -2,22 +2,22 @@
 set -e
 
 
-sed -i 's|memory_limit = 128M|memory_limit = 512M|g' /etc/php/8.2/cli/php.ini
+# sed -i 's|memory_limit = 128M|memory_limit = 512M|g' /etc/php/8.2/cli/php.ini
+if [ "$APP_ENV" != "dev" ]; then
+DIR=/var/www/agentj/vendor
 
-
-echo "Update schemas"
+if [ -d "$DIR" ] ; then
 cd /var/www/agentj &&  php bin/console doctrine:migration:migrate
-
-echo "Create or update super admin user"
 cd /var/www/agentj && php bin/console agentj:create-super-admin $SUPER_ADMIN_USERNAME $SUPER_ADMIN_PASSWORD
-
-echo "update groups wblist"
 cd /var/www/agentj && php bin/console agentj:update-groups-wblist
+fi
+
+
 
 # Allow web server user to write Symphony logs
-rm -rf /var/www/agentj/var/cache
-chown -R www-data:www-data /var/www/agentj/var
-find /var/www/agentj/public -type d -exec chmod go+rwx {} \;
+# rm -rf /var/www/agentj/var/cache
+# chown -R www-data:www-data /var/www/agentj/var
+# find /var/www/agentj/public -type d -exec chmod go+rwx {} \;
 # Allow web server user to purge old virus and spam mails
 usermod -aG www-data amavis && chmod -R g+w /tmp/amavis/quarantine
 
@@ -28,5 +28,7 @@ then
 fi
 
 cron
+if
+
 
 exec "$@"
