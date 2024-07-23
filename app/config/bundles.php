d0460de55b143607c2f07e1c4efc4b9626c538a2
Christian TRESVAUX <christian.tresvaux@probesys.com>
2024-07-23 08:05:32 +0200

    Install Symfony UI autocomplete

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -20,4 +20,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
 ];
