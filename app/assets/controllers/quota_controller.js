3a37efba2e6a5d1767884ac499193b7745acadf9
Maxime Gallo <maxime.gallo@troizaire.com>
2024-07-25 18:06:46 +0200

    all good for groups

--- app/assets/controllers/quota_controller.js
+++ app/assets/controllers/quota_controller.js
@@ -1,23 +1,36 @@
+// quota_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["container", "template"]
 
   connect() {
-    this.index = this.containerTarget.children.length
+    this.updateIndexes()
   }
 
   addQuota(event) {
     event.preventDefault()
-    const content = this.templateTarget.innerHTML.replace(/__name__/g, this.index)
+    const content = this.templateTarget.innerHTML.replace(/__name__/g, this.containerTarget.children.length)
     const element = document.createRange().createContextualFragment(content)
     this.containerTarget.appendChild(element)
-    this.index++
+    this.updateIndexes()
   }
 
   removeQuota(event) {
     event.preventDefault()
     const item = event.target.closest('.quota-item')
     item.remove()
+    this.updateIndexes()
+  }
+
+  updateIndexes() {
+    this.containerTarget.querySelectorAll('.quota-item').forEach((item, index) => {
+      item.querySelectorAll('input, select').forEach(input => {
+        const name = input.getAttribute('name')
+        if (name) {
+          input.setAttribute('name', name.replace(/\[quotas\]\[\d+\]/, `[quotas][${index}]`))
+        }
+      })
+    })
   }
 }
