50a80980ba0760273c6fd24508f8b406ac911082
Steven Despaux <steven.despaux@poste142.adm.probesys.net>
2024-09-11 16:41:22 +0200

    début création alerte, manque migration qui bug

---
+++ app/src/Controller/AlertController.php
@@ -0,0 +1,35 @@
+<?php
+// src/Controller/AlertController.php
+
+namespace App\Controller;
+
+use App\Entity\Alert;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AlertController extends AbstractController
+{
+    /**
+     * @Route("/alert/read/{id}", name="alert_read")
+     */
+    public function read(Alert $alert, EntityManagerInterface $entityManager): Response
+    {
+        $alert->setIsRead(true);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/alert/delete/{id}", name="alert_delete")
+     */
+    public function delete(Alert $alert, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($alert);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('home');
+    }
+}
