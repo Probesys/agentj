058a6d49edb889ba51c991be31e5948401c8c29f
Loïs Poujade <lois.poujade@probesys.com>
2024-09-26 11:05:22 +0200

    wip tests in ci

--- tests/test-from-host.sh
+++ tests/test-from-host.sh
@@ -107,11 +107,11 @@ send 'out_pass' 'outviarelay' 'user@laissepasser.fr' 1
 send 'in_bloc_known' 'in' 'user@blocnormal.fr' 1
 send 'in_pass_known' 'in' 'user@laissepasser.fr' 1
 
-send 'in_bloc_known_virus' 'in' 'user@blocnormal.fr' 0 "--attach @docker/tests/eicar.com.txt"
-send 'in_pass_known_virus' 'in' 'user@laissepasser.fr' 1 "--attach @docker/tests/eicar.com.txt"
+send 'in_bloc_known_virus' 'in' 'user@blocnormal.fr' 0 "--attach @tests/eicar.com.txt"
+send 'in_pass_known_virus' 'in' 'user@laissepasser.fr' 1 "--attach @tests/eicar.com.txt"
 
-send 'out_bloc_virus' 'outviarelay' 'user@blocnormal.fr' 0 "--attach @docker/tests/eicar.com.txt"
-send 'out_pass_virus' 'outviarelay' 'user@laissepasser.fr' 0 "--attach @docker/tests/eicar.com.txt"
+send 'out_bloc_virus' 'outviarelay' 'user@blocnormal.fr' 0 "--attach @tests/eicar.com.txt"
+send 'out_pass_virus' 'outviarelay' 'user@laissepasser.fr' 0 "--attach @tests/eicar.com.txt"
 
 echo "---- test don't relay from unregistered smtp ----" 1>&2
 send 'out_bloc_bad_relay' 'outviabadrelay' 'user@blocnormal.fr' 0
