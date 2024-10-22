e780faeba14bbeeaeda0f9af427e3d11fe282668
Loïs Poujade <lois.poujade@probesys.com>
2024-10-22 11:40:25 +0200

    app: fix policyd-rate-limit mail length

---
+++ app/migrations/Version20241022092208.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241022092208 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Fix policyd-rate-limit mail_count id length';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE agentj_outmail.mail_count MODIFY COLUMN id varchar(320)');
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE agentj_outmail.mail_count MODIFY COLUMN id varchar(40)');
+
+    }
+}
