da8dec31715dfc37712dd755cbafcf7a0d0e4d26
Loïs Poujade <lois.poujade@probesys.com>
2024-10-22 16:44:48 +0200

    wip tests ci

--- app/src/DataFixtures/UserFixture.php
+++ app/src/DataFixtures/UserFixture.php
@@ -17,7 +17,6 @@ class UserFixture extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-	# user@blocnormal.fr : domain quota
 	$user = new User();
 	$user->setEmail('user@blocnormal.fr');
 	$user->setFullname('user blocnormal');
@@ -29,33 +28,6 @@ class UserFixture extends Fixture
 	$user->setPolicy($user->getDomain()->getPolicy());
 	$manager->persist($user);
 
-	# unlimited@blocnormal.fr : large quota
-	$user = new User();
-	$user->setEmail('unlimited@blocnormal.fr');
-	$user->setFullname('unlimited blocnormal');
-	$user->setUsername('unlimited@blocnormal.fr');
-	$user->setRoles('["ROLE_USER"]');
-	$user->setDomain($this->em
-		->getRepository(Domain::class)
-		->findOneBy(['domain' => 'blocnormal.fr']));
-	$user->setPolicy($user->getDomain()->getPolicy());
-	$user->setQuota([["quota_emails" => 1000, "quota_seconds" => 1]]);
-	$manager->persist($user);
-
-	# limited@blocnormal.fr : large quota
-	$user = new User();
-	$user->setEmail('limited@blocnormal.fr');
-	$user->setFullname('limited blocnormal');
-	$user->setUsername('limited@blocnormal.fr');
-	$user->setRoles('["ROLE_USER"]');
-	$user->setDomain($this->em
-		->getRepository(Domain::class)
-		->findOneBy(['domain' => 'blocnormal.fr']));
-	$user->setPolicy($user->getDomain()->getPolicy());
-	$user->setQuota([["quota_emails" => 1, "quota_seconds" => 1]]);
-	$manager->persist($user);
-
-	# user@laissepasser.fr : no quota
 	$user = new User();
 	$user->setEmail('user.domain.quota@blocnormal.fr');
 	$user->setFullname('userDomainQuota blocnormal');
