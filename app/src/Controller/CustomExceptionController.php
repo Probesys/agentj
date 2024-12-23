4a771fc007b65528f1c5f42962d82f2dfeb3dc13
Steven Despaux <steven.despaux@poste156.adm.probesys.net>
2024-12-23 17:24:56 +0100

    removed useless stuff from controller when testing

--- app/src/Controller/CustomExceptionController.php
+++ app/src/Controller/CustomExceptionController.php
@@ -7,24 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Twig\Environment;
 
-
-use App\Controller\MessageController;
-use App\Model\ValidationSource;
-use App\Entity\Domain;
-use App\Entity\MessageStatus;
-use App\Entity\Msgrcpt;
-use App\Entity\Msgs;
-use App\Entity\User;
-use App\Entity\Alert;
-use App\Form\CaptchaFormType;
-use App\Service\CryptEncryptService;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
-
 class CustomExceptionController extends AbstractController
 {
     private $twig;
