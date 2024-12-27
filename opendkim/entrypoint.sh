0b42bced48345f38fd41af4e0efe61cf47d73a40
Loïs Poujade <lois.poujade@probesys.com>
2024-12-27 16:04:55 +0100

    db/opendkim: create a separate read only user

--- opendkim/entrypoint.sh
+++ opendkim/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 sed -i "s/\$DB_HOST/$DB_HOST/g" /etc/opendkim.conf
 sed -i "s/\$DB_NAME/$DB_NAME/g" /etc/opendkim.conf
 sed -i "s/\$DB_USER/$DB_USER/g" /etc/opendkim.conf
-sed -i "s/\$DB_PASSWORD/$DB_PASSWORD/g" /etc/opendkim.conf
+sed -i "s/\$DB_OPENDKIM_PASSWORD/$DB_OPENDKIM_PASSWORD/g" /etc/opendkim.conf
 
 IPV4_NETWORK=$(ip route | grep  kernel | awk '{ print $1}')
 sed -i "s~\$IPV4_NETWORK~$IPV4_NETWORK~g" /etc/trusted.opendkim.conf
