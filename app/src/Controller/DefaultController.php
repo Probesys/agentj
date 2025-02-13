2c9085c8fa6757147181fa6f4a33589be751ad9d
Marien Fressinaud <marien.fressinaud@probesys.coop>
2025-02-13 11:57:28 +0100

    refactor(app): Extract a MessageService
    All the logic to authorize / ban / release messages was contained in the
    MessageController with complicated methods. They were used by other
    controllers leading to code hard to maintain.

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -11,6 +11,7 @@ use App\Entity\Msgs;
 use App\Entity\User;
 use App\Entity\Alert;
 use App\Form\CaptchaFormType;
+use App\Service;
 use App\Service\CryptEncryptService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -222,73 +223,46 @@ class DefaultController extends AbstractController {
     }
 
     #[Route(path: '/check/{token}', name: 'check_message', methods: 'GET|POST')]
-    public function checkCaptcha($token, Request $request, MessageController $messageController, CryptEncryptService $cryptEncrypt) {
+    public function checkCaptcha($token, Request $request, Service\MessageService $messageService, CryptEncryptService $cryptEncrypt) {
         $em = $this->em;
         $confirm = "";
 
-        $decrypt = $cryptEncrypt->decrypt($token);
-        $mailId = null;
-        $date = null;
-        $rid = null;
-        if (isset($decrypt[0]) && isset($decrypt[1])) {
-            $date = $decrypt[0];
-            $msg = explode('%%%', $decrypt[1]);
-            if (isset($msg[0]) && isset($msg[1])) {
-                $mailId = $msg[0];
-                if (isset($msg[0]) && isset($msg[4])) {
-                    $rid = $msg[4];
-                }
-                $partitionTag = $msg[2];
-            } else {
-                throw $this->createNotFoundException('This page does not exist');
-            }
+        list($message, $domain, $messageRecipients) = $this->decryptMessageToken($cryptEncrypt, $token);
+
+        if (!$message) {
+            throw $this->createNotFoundException('The message does not exist.');
         }
 
-        if (!$mailId) {
-            throw $this->createNotFoundException('This page does not exist');
-        }
-
-        /* @var $msgs Msgs */
-        $msgs = $this->em->getRepository(Msgs::class)->findOneBy(['mailId' => $mailId]);
-
-        if (!$msgs) {
-            throw $this->createNotFoundException('This page does not exist');
-        }
-
-        $content = "";
-        $domainId = $msg[3]; //need domain Id to get message
-        $domain = $em->getRepository(Domain::class)->find($domainId);
         if (!$domain) {
-            throw $this->createNotFoundException('This page does not exist');
+            throw $this->createNotFoundException('The domain does not exist.');
         }
 
-        $senderEmail = stream_get_contents($msgs->getSid()->getEmail(), -1, 0);
-        if ($domain) {
-            $content = !empty($domain->getMessage()) ? $domain->getMessage() : $this->translator->trans('Message.Captcha.defaultCaptchaPageContent');
-        }
+        $senderEmail = stream_get_contents($message->getSid()->getEmail(), -1, 0);
+        $content = !empty($domain->getMessage()) ? $domain->getMessage() : $this->translator->trans('Message.Captcha.defaultCaptchaPageContent');
 
         $form = $this->createForm(CaptchaFormType::class, null);
         $form->handleRequest($request); //todo gérer l'erreur si la page est rechargée
+
         if ($form->isSubmitted() && $form->isValid()) {
             $confirm = !empty($domain->getConfirmCaptchaMessage()) ? $domain->getConfirmCaptchaMessage() : $this->translator->trans('Message.Captcha.defaultCaptchaConfirmMsg');
-            $recipient = $em->getRepository(Msgrcpt::class)->findOneBy(['mailId' => $mailId]);
+            $recipient = $messageRecipients[0] ?? null;
             if ($recipient) {
                 $confirm = str_replace('[EMAIL_DEST]', stream_get_contents($recipient->getRid()->getEmail(), -1, 0), $confirm);
             }
-                if (!$msgs->getStatus() && $form->has('email') && $form->get('email')->getData() == $senderEmail) { // Test is the sender is the same than the posted email field and if the mail has not been yet treated
-                    if ($form->has('emailEmpty') && empty($form->get('emailEmpty')->getData())) { // Test HoneyPot
-                        $messageController->msgsToWblist($partitionTag, $mailId, "W", MessageStatus::AUTHORIZED, ValidationSource::captcha, $rid);
-                        $msgs->setValidateCaptcha(time());
-                        $em->persist($msgs);
-                        $em->flush();
-                        $mailId = stream_get_contents($msgs->getMailId(), -1, 0);
-                    } else {
-                        $this->addFlash('error', $this->translator->trans('Message.Flash.checkMailUnsuccessful'));
-                    }
+
+            if (!$message->getStatus() && $form->has('email') && $form->get('email')->getData() == $senderEmail) { // Test is the sender is the same than the posted email field and if the mail has not been yet treated
+                if ($form->has('emailEmpty') && empty($form->get('emailEmpty')->getData())) { // Test HoneyPot
+                    $messageService->authorize($message, $messageRecipients, ValidationSource::captcha);
+                    $message->setValidateCaptcha(time());
+                    $em->persist($message);
+                    $em->flush();
+                    $mailId = stream_get_contents($message->getMailId(), -1, 0);
                 } else {
                     $this->addFlash('error', $this->translator->trans('Message.Flash.checkMailUnsuccessful'));
                 }
-
+            } else {
+                $this->addFlash('error', $this->translator->trans('Message.Flash.checkMailUnsuccessful'));
+            }
         } else {
             $form->get('email')->setData($senderEmail);
         }
@@ -337,4 +311,35 @@ class DefaultController extends AbstractController {
         return new RedirectResponse($url);
     }
 
+    /**
+     * @return array{?Msgs, ?Domain, Msgrcpt[]}
+     */
+    private function decryptMessageToken(CryptEncryptService $cryptEncryptService, string $token): array
+    {
+        list($expirationTimestamp, $decryptedToken) = $cryptEncryptService->decrypt($token);
+
+        $tokenParts = explode('%%%', $decryptedToken);
+
+        if (count($tokenParts) < 4) {
+            throw $this->createNotFoundException('The token is invalid.');
+        }
+
+        $mailId = $tokenParts[0];
+        $secretId = $tokenParts[1];
+        $partitionTag = $tokenParts[2];
+        $domainId = $tokenParts[3];
+        $rid = $tokenParts[4] ?? null;
+
+        $message = $this->em->getRepository(Msgs::class)->findOneByMailId($partitionTag, $mailId);
+        $domain = $this->em->getRepository(Domain::class)->find($domainId);
+
+        $messageRecipients = [];
+        if ($message && $rid === null) {
+            $messageRecipients = $this->em->getRepository(Msgrcpt::class)->findByMessage($message);
+        } elseif ($message) {
+            $messageRecipients[] = $this->em->getRepository(Msgrcpt::class)->findOneByMessageAndRid($message, $rid);
+        }
+
+        return [$message, $domain, $messageRecipients];
+    }
 }
