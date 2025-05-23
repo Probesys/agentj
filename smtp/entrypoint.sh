6fba527e55fbdeabfd518785747e14307c7f6601
Loïs Poujade <lois.poujade@probesys.coop>
2025-05-23 16:03:47 +0200

    smtp: ease external relays integration
    Closes #188

--- smtp/entrypoint.sh
+++ smtp/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-IPV4_NETWORK=$(ip route | grep  kernel | awk '{ print $1}')
+IPV4_NETWORK="$(ip route | grep  kernel | awk '{ print $1}') $SMTP_TRUSTED_PROXIES"
 sed -i "s~\$IPV4_NETWORK~$IPV4_NETWORK~g" /etc/postfix-*/main.cf
 sed -i "s~\$IPV4_NETWORK~$IPV4_NETWORK~g" /etc/postfix-*/master.cf
 
@@ -9,6 +9,10 @@ sed -i "s~\$IPV4_NETWORK~$IPV4_NETWORK~g" /etc/postfix-*/master.cf
 sed -i "s/\$DOMAIN/$DOMAIN/g" /etc/postfix-*/main.cf
 sed -i "s/\$EHLO_DOMAIN/${EHLO_DOMAIN:-$DOMAIN}/g" /etc/postfix-*/main.cf
 echo "$DOMAIN" > /etc/mailname
+
+# update relayhost
+sed -i "s~\$SMTP_OUT_RELAY~$SMTP_OUT_RELAY~g" /etc/postfix-*/master.cf
+
 
 postmap "lmdb:/etc/postfix-common/slow_dest_domains_transport"
 sed -i "s/\$DB_NAME/$DB_NAME/g" /etc/postfix-*/mysql-*.cf
