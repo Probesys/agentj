5a97b3b50eab85fcdc3bc5bcf2a13610a29420b5
Marien Fressinaud <marien.fressinaud@probesys.coop>
2025-07-18 10:11:29 +0200

    chore(db): Upgrade to MariaDB 11.4

--- mariadb-entrypoint-initdb.d/opendkim_user.sh
+++ mariadb-entrypoint-initdb.d/opendkim_user.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" \
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" \
 	-e "create user 'opendkim' identified by '$DB_OPENDKIM_PASSWORD'; grant select on $MYSQL_DATABASE.* to 'opendkim';"
