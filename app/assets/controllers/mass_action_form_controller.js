a48460e61e48aaea1be8d6b303dd61c8f0bbb70a
ctresvaux <ctresvaux@hotmail.com>
2025-08-29 10:45:11 +0200

    Update app/assets/controllers/mass_action_form_controller.js
    Co-authored-by: Marien Fressinaud (pro account) <107053378+marien-probesys@users.noreply.github.com>

--- app/assets/controllers/mass_action_form_controller.js
+++ app/assets/controllers/mass_action_form_controller.js
@@ -2,7 +2,8 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
 
-    submit () {
+    submit (event) {
+        event.preventDefault();
         const confirmMessage = this.element.dataset.messageConfirm;
         const checkboxesGroup = this.element.dataset.massActionGroup;
         const checkboxes = document.querySelectorAll(`input[type='checkbox'][data-mass-action-group="${checkboxesGroup}":checked`);
