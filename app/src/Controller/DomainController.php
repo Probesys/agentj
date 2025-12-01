Probesys/agentj

commit fe6d0cbe9fbcf95f2b14326b89898bec8343efc2

feat: Change the domain "messages" in a "customisation" section

app/src/Controller/DomainController.php
@@ -9,12 +9,14 @@ use App\Entity\Mailaddr;
 use App\Entity\Policy;
 use App\Entity\User;
 use App\Entity\Wblist;
+use App\Form\DomainCustomisationGeneralType;
+use App\Form\DomainCustomisationHumanAuthenticationType;
+use App\Form\DomainCustomisationReportType;
 use App\Form\DomainMessageType;
 use App\Form\DomainType;
 use App\Model\ConnectorTypes;
 use App\Repository\SettingsRepository;
 use App\Repository\DomainRepository;
-use App\Service\FileUploader;
 use App\Service\MailaddrService;
 use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -94,7 +96,6 @@ class DomainController extends AbstractController
     #[Route(path: '/new', name: 'domain_new', methods: 'GET|POST')]
     public function new(
         Request $request,
-        FileUploader $fileUploader,
         ParameterBagInterface $params,
         SettingsRepository $settingsRepository,
     ): Response {
@@ -175,15 +176,6 @@ class DomainController extends AbstractController
             $wblist->setPriority(Wblist::WBLIST_PRIORITY_DOMAIN);
             $this->em->persist($wblist);
 
-            if ($form->has('logoFile')) {
-                $uploadedFile = $form['logoFile']->getData();
-                if ($uploadedFile) {
-                    $uploadedLogo = $fileUploader->upload($uploadedFile);
-                    $domain->setLogo($uploadedLogo);
-                }
-            }
-
-
             $this->em->flush();
             $this->addFlash('success', $this->translator->trans('Message.Flash.domainCreatd'));
             return $this->redirectToRoute('domain_edit', ['id' => $domain->getId()]);
@@ -210,7 +202,7 @@ class DomainController extends AbstractController
     }
 
     #[Route(path: '/{id}/edit', name: 'domain_edit', methods: 'GET|POST')]
-    public function edit(Request $request, Domain $domain, FileUploader $fileUploader): Response
+    public function edit(Request $request, Domain $domain): Response
     {
 
         $this->checkAccess($domain);
@@ -253,17 +245,6 @@ class DomainController extends AbstractController
             $rules = $form->get("rules")->getData();
             $wblist->setWb($rules);
             $wblist->setPriority(Wblist::WBLIST_PRIORITY_DOMAIN);
-
-            // Update users with domain policy
-
-
-            if ($form->has('logoFile')) {
-                $uploadedFile = $form['logoFile']->getData();
-                if ($uploadedFile) {
-                    $uploadedLogo = $fileUploader->upload($uploadedFile);
-                    $domain->setLogo($uploadedLogo);
-                }
-            }
 
             if ($domain->getDomainKeys() === null) {
                 $this->generateOpenDkim($domain);
@@ -399,25 +380,6 @@ class DomainController extends AbstractController
         }
 
         return $this->render('domain/newwblist.html.twig', [
-            'domain' => $domain,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    #[Route(path: '/{id}/messages', name: 'domain_messages', methods: 'GET|POST')]
-    public function domainMessages(Request $request, Domain $domain): Response
-    {
-        $this->checkAccess($domain);
-        $form = $this->createForm(DomainMessageType::class, $domain);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->em->flush();
-
-            return $this->redirectToRoute('domain_index', ['id' => $domain->getId()]);
-        }
-
-        return $this->render('domain/messages.html.twig', [
             'domain' => $domain,
             'form' => $form->createView(),
         ]);

app/assets/controllers/ckeditor_controller.js
@@ -1,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    connect() {
+        this.editor = CKEDITOR.replace(this.element);
+    }
+
+    disconnect() {
+        if (this.editor) {
+            CKEDITOR.remove(this.editor);
+        }
+    }
+}

app/assets/controllers/ckeditor_variables_controller.js
@@ -1,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    insert (event) {
+        if (!CKEDITOR.currentInstance) {
+            return;
+        }
+
+        const variable = event.currentTarget.dataset.variable;
+        if (!variable) {
+            console.error('data-variable is undefined on element');
+            return;
+        }
+
+        if (variable.startsWith('[URL_')) {
+            CKEDITOR.currentInstance.insertHtml(
+                `<a href="${variable}">${variable}</a>`
+            );
+        } else {
+            CKEDITOR.currentInstance.insertHtml(variable);
+        }
+    }
+}

app/config/packages/twig.yaml
@@ -15,17 +15,6 @@ twig:
             - fr
             - en
         per_pages: [10, 25, 50, 100, 500, 1000]
-        domainMessages_availableFields:
-            'Entities.Domain.labels.dynamicsVariables.URL_HUMAN_AUTHENTICATION' : 'URL_HUMAN_AUTHENTICATION'
-            'Entities.Domain.labels.dynamicsVariables.EMAIL_DEST' : 'EMAIL_DEST'
-            'Entities.Domain.labels.dynamicsVariables.URL_MSGS' : 'URL_MSGS'
-            'Entities.Domain.labels.dynamicsVariables.NB_UNTREATED_MESSAGES' : 'NB_UNTREATED_MESSAGES'
-            'Entities.Domain.labels.dynamicsVariables.NB_AUTHORIZED_MESSAGES' : 'NB_AUTHORIZED_MESSAGES'
-            'Entities.Domain.labels.dynamicsVariables.NB_SPAMMED_MESSAGES' : 'NB_SPAMMED_MESSAGES'
-            'Entities.Domain.labels.dynamicsVariables.NB_BANNED_MESSAGES' : 'NB_BANNED_MESSAGES'
-            'Entities.Domain.labels.dynamicsVariables.NB_DELETED_MESSAGES' : 'NB_DELETED_MESSAGES'
-            'Entities.Domain.labels.dynamicsVariables.NB_RESTORED_MESSAGES' : 'NB_RESTORED_MESSAGES'
-            'Entities.Domain.labels.dynamicsVariables.LIST_MAIL_MSGS' : 'LIST_MAIL_MSGS'
 
 when@test:
     twig:

app/src/Controller/Domain/CustomisationController.php
@@ -1,0 +1,149 @@
+<?php
+
+namespace App\Controller\Domain;
+
+use App\Entity\Domain;
+use App\Entity\User;
+use App\Form\DomainCustomisationGeneralType;
+use App\Form\DomainCustomisationHumanAuthenticationType;
+use App\Form\DomainCustomisationReportType;
+use App\Repository\DomainRepository;
+use App\Service\FileUploader;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Translation\TranslatableMessage;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class CustomisationController extends AbstractController
+{
+    public function __construct(
+        private DomainRepository $domainRepository,
+        private TranslatorInterface $translator,
+    ) {
+    }
+
+    #[Route(path: '/domain/{id}/customisation', name: 'domain_customisation_general', methods: ['GET', 'POST'])]
+    public function general(Request $request, Domain $domain, FileUploader $fileUploader): Response
+    {
+        $this->checkAccess($domain);
+
+        $form = $this->createForm(DomainCustomisationGeneralType::class, $domain);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $domain = $form->getData();
+
+            $uploadedFile = $form->get('logoFile')->getData();
+            if ($uploadedFile) {
+                $uploadedLogo = $fileUploader->upload($uploadedFile);
+                $domain->setLogo($uploadedLogo);
+            }
+
+            $this->domainRepository->save($domain);
+
+            $this->addFlash('success', $this->translator->trans('Message.Flash.saved'));
+
+            return $this->redirectToRoute('domain_customisation_general', [
+                'id' => $domain->getId(),
+            ]);
+        }
+
+        return $this->render('domain/customisation/general.html.twig', [
+            'domain' => $domain,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route(
+        path: '/domain/{id}/customisation/human-authentication',
+        name: 'domain_customisation_human_authentication',
+        methods: ['GET', 'POST'],
+    )]
+    public function humanAuthentication(Request $request, Domain $domain): Response
+    {
+        $this->checkAccess($domain);
+
+        $form = $this->createForm(DomainCustomisationHumanAuthenticationType::class, $domain);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $domain = $form->getData();
+
+            $this->domainRepository->save($domain);
+
+            $this->addFlash('success', $this->translator->trans('Message.Flash.saved'));
+
+            return $this->redirectToRoute('domain_customisation_human_authentication', [
+                'id' => $domain->getId(),
+            ]);
+        }
+
+        return $this->render('domain/customisation/human_authentication.html.twig', [
+            'domain' => $domain,
+            'form' => $form,
+            // phpcs:disable Generic.Files.LineLength
+            'availableVariables' => [
+                'URL_HUMAN_AUTHENTICATION' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.URL_HUMAN_AUTHENTICATION'),
+                'EMAIL_DEST' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.EMAIL_DEST'),
+            ],
+            // phpcs:enable Generic.Files.LineLength
+        ]);
+    }
+
+    #[Route(path: '/domain/{id}/customisation/report', name: 'domain_customisation_report', methods: ['GET', 'POST'])]
+    public function report(Request $request, Domain $domain): Response
+    {
+        $this->checkAccess($domain);
+
+        $form = $this->createForm(DomainCustomisationReportType::class, $domain);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $domain = $form->getData();
+
+            $this->domainRepository->save($domain);
+
+            $this->addFlash('success', $this->translator->trans('Message.Flash.saved'));
+
+            return $this->redirectToRoute('domain_customisation_report', [
+                'id' => $domain->getId(),
+            ]);
+        }
+
+        return $this->render('domain/customisation/report.html.twig', [
+            'domain' => $domain,
+            'form' => $form,
+            // phpcs:disable Generic.Files.LineLength
+            'availableVariables' => [
+                'USERNAME' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.USERNAME'),
+                'URL_MSGS' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.URL_MSGS'),
+                'NB_UNTREATED_MESSAGES' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.NB_UNTREATED_MESSAGES'),
+                'NB_AUTHORIZED_MESSAGES' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.NB_AUTHORIZED_MESSAGES'),
+                'NB_SPAMMED_MESSAGES' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.NB_SPAMMED_MESSAGES'),
+                'NB_BANNED_MESSAGES' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.NB_BANNED_MESSAGES'),
+                'NB_DELETED_MESSAGES' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.NB_DELETED_MESSAGES'),
+                'NB_RESTORED_MESSAGES' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.NB_RESTORED_MESSAGES'),
+                'LIST_MAIL_MSGS' => new TranslatableMessage('Entities.Domain.labels.dynamicsVariables.LIST_MAIL_MSGS'),
+            ],
+            // phpcs:enable Generic.Files.LineLength
+        ]);
+    }
+
+    private function checkAccess(Domain $domain): void
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $hasAccess = (
+            $user->isSuperAdmin() ||
+            ($user->isAdmin() && $user->hasDomain($domain))
+        );
+
+        if (!$hasAccess) {
+            throw new AccessDeniedException();
+        }
+    }
+}

app/src/Form/DomainCustomisationGeneralType.php
@@ -1,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Domain;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DomainCustomisationGeneralType extends AbstractType
+{
+    /**
+     * @var array<string, string>
+     */
+    private array $tabLanguages;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $langs = explode('|', $params->get('app_locales'));
+        foreach ($langs as $lang) {
+            $this->tabLanguages[$lang] = $lang;
+        }
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('defaultLang', ChoiceType::class, [
+            'choices' => $this->tabLanguages,
+            'label' => 'Entities.Domain.fields.defaultLang',
+            'required' => false,
+        ]);
+
+        $builder->add('logoFile', FileType::class, [
+            'label' => 'Logo',
+            'mapped' => false,
+            'required' => false,
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Domain::class,
+        ]);
+    }
+}

app/src/Form/DomainCustomisationHumanAuthenticationType.php
@@ -1,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Domain;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DomainCustomisationHumanAuthenticationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('mailmessage', TextareaType::class, [
+            'label' => 'Entities.Domain.fields.mailmessage',
+            'attr' => [
+                'data-controller' => 'ckeditor',
+            ],
+        ]);
+        $builder->add('message', TextareaType::class, [
+            'label' => 'Entities.Domain.fields.message',
+            'attr' => [
+                'data-controller' => 'ckeditor',
+            ],
+        ]);
+        $builder->add('confirmCaptchaMessage', TextareaType::class, [
+            'label' => 'Entities.Domain.fields.confirmCaptchaMessage',
+            'attr' => [
+                'data-controller' => 'ckeditor',
+            ],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Domain::class,
+        ]);
+    }
+}

app/src/Form/DomainCustomisationReportType.php
@@ -1,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Domain;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DomainCustomisationReportType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('messageAlert', TextareaType::class, [
+            'label' => 'Entities.Domain.fields.messageAlert',
+            'attr' => [
+                'data-controller' => 'ckeditor',
+            ],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Domain::class,
+        ]);
+    }
+}

commit df1c7e9e5a557c695a37796af4dd31c9ccd7cf5c

fix: Fix domain creation
The bug was introduced by commit fe6d0cbe, after that we removed the
defaultLang from the DomainType form.

app/src/Controller/DomainController.php
@@ -111,7 +111,6 @@ class DomainController extends AbstractController
             'minSpamLevel' => $this->getParameter('app.domain_min_spam_level'),
             'maxSpamLevel' => $this->getParameter('app.domain_max_spam_level'),
         ]);
-        $form->get('defaultLang')->setData($params->get('locale'));
 
         // set normal policy anf "block all mails" as default rule for new domain
         $policy = $this->em->getRepository(Policy::class)->find(5);