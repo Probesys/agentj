8bae2559bb6ed56cbb94319a2d79ad4a3abafc80
Christian TRESVAUX <christian.tresvaux@probesys.com>
2025-12-05 17:24:06 +0100

    chore : move dashboard templates to dedicated folder

--- app/src/Controller/DashBoardController.php
+++ app/src/Controller/DashBoardController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Amavis\ContentType;
 use App\Amavis\MessageStatus;
 use App\Repository\AlertRepository;
 use App\Repository\DomainRepository;
+use App\Repository\MsgrcptRepository;
 use App\Repository\MsgrcptSearchRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,6 +17,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DashBoardController extends AbstractController
 {
     public function __construct(
+        private EntityManagerInterface $em,
         private MsgrcptSearchRepository $msgrcptSearchRepository,
     ) {
     }
@@ -32,7 +36,7 @@ final class DashBoardController extends AbstractController
             return !$alert->getIsRead();
         }));
 
-        return $this->render('home/alerts.html.twig', [
+        return $this->render('dashboard/alerts.html.twig', [
             'alerts' => $alerts,
             'unreadAlertsCount' => $unreadAlertsCount,
         ]);
@@ -59,7 +63,7 @@ final class DashBoardController extends AbstractController
         $msgs['spammed'] = $this->msgrcptSearchRepository->countByType($user, MessageStatus::SPAMMED);
         $msgs['virus'] = $this->msgrcptSearchRepository->countByType($user, MessageStatus::VIRUS);
 
-        return $this->render('home/stats-by-status.html.twig', [
+        return $this->render('dashboard/stats-by-status.html.twig', [
             'msgs' => $msgs,
             'latestMessageRecipients' => $latestMessageRecipients,
         ]);
@@ -103,7 +107,7 @@ final class DashBoardController extends AbstractController
             messageStatus: MessageStatus::VIRUS
         );
 
-        return $this->render('home/stats-by-days.html.twig', [
+        return $this->render('dashboard/stats-by-days.html.twig', [
             'nbUntreadtedMsgByDay' => $nbUntreadtedMsgByDay,
             'nbAutorizeMsgByDay' => $nbAutorizeMsgByDay,
             'nbBannedMsgByDay' => $nbBannedMsgByDay,
@@ -173,11 +177,99 @@ final class DashBoardController extends AbstractController
             }, 0),
         ];
 
-        return $this->render('home/stats-by-in-out.html.twig', [
+        return $this->render('dashboard/stats-by-in-out.html.twig', [
             'latestMessageRecipients' => $latestMessageRecipients,
             'domains' => $domains,
             'users' => $users,
             'data' => $data,
         ]);
+    }
+
+    #[Route(path: '{userEmail}/messages_stats/', name: 'messages_stats', methods: 'GET')]
+    public function showMessagesStats(
+        string $userEmail,
+        MsgrcptRepository $msgrcptRepository,
+        UserRepository $userRepository
+    ): Response {
+        $connection = $this->em->getConnection();
+
+        $user = $userRepository->findOneBy(['email' => $userEmail]);
+
+        $query = $msgrcptRepository->findByEmailRecipient($userEmail);
+        $messages = $query->getArrayResult();
+
+        $sqlOutMessages = '
+            SELECT out_msgs.*, mr.status_id as status, mr.bspam_level as bspamLevel, mr.content
+            FROM out_msgs
+            JOIN out_msgrcpt AS mr ON out_msgs.mail_id = mr.mail_id
+            WHERE out_msgs.from_addr = :userEmail
+        ';
+        $stmtOutMessages = $connection->executeQuery($sqlOutMessages, ['userEmail' => $userEmail]);
+        $outMessages = $stmtOutMessages->fetchAllAssociative();
+
+        // Determine status using provided logic
+        $statusCounts = [];
+
+        foreach ($messages as $message) {
+            $status = $this->determineStatus($message, $user->getDomain()->getLevel());
+            if (!isset($statusCounts[$status])) {
+                $statusCounts[$status] = ['name' => $status, 'qty' => 0, 'qty_out' => 0];
+            }
+            $statusCounts[$status]['qty']++;
+        }
+
+        foreach ($outMessages as $outMessage) {
+            $status = $this->determineStatus($outMessage, $user->getDomain()->getLevel());
+            if (!isset($statusCounts[$status])) {
+                $statusCounts[$status] = ['name' => $status, 'qty' => 0, 'qty_out' => 0];
+            }
+            if ($status != 'untreated') {
+                $statusCounts[$status]['qty_out']++;
+            }
+        }
+
+        // Convert to array and return to view
+        $messagesStatus = array_values($statusCounts);
+
+        // Get the count of sql_limit_report entries where id = $userEmail
+        $sqlLimitReports = '
+            SELECT COUNT(*) AS count
+            FROM sql_limit_report
+            WHERE mail_id = :userEmail
+        ';
+        $stmtLimitReports = $connection->executeQuery($sqlLimitReports, ['userEmail' => $userEmail]);
+        $limitReports = $stmtLimitReports->fetchAssociative();
+
+        // Add Sql_Limit_Report to $messagesStatus
+        $messagesStatus[] = [
+            'name' => 'quota',
+            'qty' => 0,
+            'qty_out' => $limitReports ? $limitReports['count'] : 0
+        ];
+
+        return $this->render('dashboard/messages_stats.html.twig', [
+            'msgs_status' => $messagesStatus,
+        ]);
+    }
+
+    /**
+     * Determine the status of a message based on its properties
+     * @param array<string, mixed> $message
+     * @return string
+     */
+    private function determineStatus(array $message, float $level): string
+    {
+        if ($message['status'] !== null) {
+            return MessageStatus::getStatusName($message['status']);
+        } elseif (
+            $message['status'] === null && $message['bspamLevel'] > $level
+            && $message['content'] !== ContentType::CLEAN && $message['content'] !== ContentType::VIRUS
+        ) {
+            return 'spam';
+        } elseif ($message['content'] === ContentType::VIRUS) {
+            return 'virus';
+        } else {
+            return 'untreated';
+        }
     }
 }
