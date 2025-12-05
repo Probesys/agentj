b25a86bbd5383e4ed13db34aa200c3a8d7cde07b
Christian TRESVAUX <christian.tresvaux@probesys.com>
2025-12-05 17:24:06 +0100

    chore : move dashboard templates to dedicated folder

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -18,7 +18,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     public function __construct(
-        private EntityManagerInterface $em,
         private Security $security,
     ) {
     }
@@ -39,93 +38,6 @@ class DefaultController extends AbstractController
         ]);
     }
 
-    #[Route(path: '{userEmail}/messages_stats/', name: 'messages_stats', methods: 'GET')]
-    public function showMessagesStats(
-        string $userEmail,
-        MsgrcptRepository $msgrcptRepository,
-        UserRepository $userRepository
-    ): Response {
-        $connection = $this->em->getConnection();
-
-        $user = $userRepository->findOneBy(['email' => $userEmail]);
-
-        $query = $msgrcptRepository->findByEmailRecipient($userEmail);
-        $messages = $query->getArrayResult();
-
-        $sqlOutMessages = '
-            SELECT out_msgs.*, mr.status_id as status, mr.bspam_level as bspamLevel, mr.content
-            FROM out_msgs
-            JOIN out_msgrcpt AS mr ON out_msgs.mail_id = mr.mail_id
-            WHERE out_msgs.from_addr = :userEmail
-        ';
-        $stmtOutMessages = $connection->executeQuery($sqlOutMessages, ['userEmail' => $userEmail]);
-        $outMessages = $stmtOutMessages->fetchAllAssociative();
-
-        // Determine status using provided logic
-        $statusCounts = [];
-
-        foreach ($messages as $message) {
-            $status = $this->determineStatus($message, $user->getDomain()->getLevel());
-            if (!isset($statusCounts[$status])) {
-                $statusCounts[$status] = ['name' => $status, 'qty' => 0, 'qty_out' => 0];
-            }
-            $statusCounts[$status]['qty']++;
-        }
-
-        foreach ($outMessages as $outMessage) {
-            $status = $this->determineStatus($outMessage, $user->getDomain()->getLevel());
-            if (!isset($statusCounts[$status])) {
-                $statusCounts[$status] = ['name' => $status, 'qty' => 0, 'qty_out' => 0];
-            }
-            if ($status != 'untreated') {
-                $statusCounts[$status]['qty_out']++;
-            }
-        }
-
-        // Convert to array and return to view
-        $messagesStatus = array_values($statusCounts);
-
-        // Get the count of sql_limit_report entries where id = $userEmail
-        $sqlLimitReports = '
-            SELECT COUNT(*) AS count
-            FROM sql_limit_report
-            WHERE mail_id = :userEmail
-        ';
-        $stmtLimitReports = $connection->executeQuery($sqlLimitReports, ['userEmail' => $userEmail]);
-        $limitReports = $stmtLimitReports->fetchAssociative();
-
-        // Add Sql_Limit_Report to $messagesStatus
-        $messagesStatus[] = [
-            'name' => 'quota',
-            'qty' => 0,
-            'qty_out' => $limitReports ? $limitReports['count'] : 0
-        ];
-
-        return $this->render('home/messages_stats.html.twig', [
-            'msgs_status' => $messagesStatus,
-        ]);
-    }
-
-    /**
-     * Determine the status of a message based on its properties
-     * @param array<string, mixed> $message
-     * @return string
-     */
-    private function determineStatus(array $message, float $level): string
-    {
-        if ($message['status'] !== null) {
-            return MessageStatus::getStatusName($message['status']);
-        } elseif (
-            $message['status'] === null && $message['bspamLevel'] > $level
-            && $message['content'] !== ContentType::CLEAN && $message['content'] !== ContentType::VIRUS
-        ) {
-            return 'spam';
-        } elseif ($message['content'] === ContentType::VIRUS) {
-            return 'virus';
-        } else {
-            return 'untreated';
-        }
-    }
 
     /**
      * Change the locale for the current user.
