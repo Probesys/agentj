f49c0541c2b5c602a91d1770b29a523b30c5c53d
Christian TRESVAUX <christian.tresvaux@probesys.com>
2025-12-17 16:46:38 +0100

    chore: Add a command to "auto release" messages

---
+++ app/src/Command/AmavisAutoReleaseCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Command;
+
+use App\Amavis\MessageStatus;
+use App\Repository\MsgrcptSearchRepository;
+use App\Repository\UserRepository;
+use App\Service\MessageService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\Store\SemaphoreStore;
+
+#[AsCommand(
+    name: 'agentj:auto-release-message',
+    description: 'Release untreated messages for users with bypass_human_auth enabled',
+)]
+class AmavisAutoReleaseCommand extends Command
+{
+    private int $batchSize = 1000;
+
+    public function __construct(
+        private MsgrcptSearchRepository $msgrcptSearchRepository,
+        private UserRepository $userRepository,
+        private MessageService $messageService,
+    ) {
+        parent::__construct();
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        $store = new SemaphoreStore();
+        $factory = new LockFactory($store);
+        $lock = $factory->createLock('msgs-auto-release', 1800);
+
+        if (!$lock->acquire()) {
+            $output->writeln("Can't acquire the msgs-auto-release lock, the command is probably already running.");
+            return Command::FAILURE;
+        }
+
+        $searchQuery = $this->msgrcptSearchRepository->getSearchQuery(
+            null,
+            MessageStatus::UNTREATED
+        );
+
+        $messageRecipients = $searchQuery->setMaxResults($this->batchSize)->getResult();
+
+        foreach ($messageRecipients as $messageRecipient) {
+            if ($messageRecipient->isAmavisReleaseOngoing()) {
+                continue;
+            }
+
+            $user = $this->userRepository->findOneBy([
+                'email' => $messageRecipient->getRid()->getEmailClear()
+            ]);
+
+            if (!$user) {
+                continue;
+            }
+
+            if ($user->getBypassHumanAuth()) {
+                $this->messageService->dispatchRelease($messageRecipient);
+            }
+        }
+
+        return Command::SUCCESS;
+    }
+}
