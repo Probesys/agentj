65ba904e972a05001e2319f6708d56ab40b5dc38
Marien Fressinaud <marien.fressinaud@probesys.coop>
2025-12-18 15:05:13 +0100

    fix: Fix setting the locale
    Two problems happened. First, the `Accept-Language` header was ignored
    when the user wasn't logged-in. It means that the French locale was
    always used. The refactor of the LocaleSubscriber fixes this.
    
    The second problem was that once the session expired, but that the user
    was logged in again through the "remember me" cookie, the locale wasn't
    set anymore. This is why I introduce a UserLocaleSubscriber which is
    triggered on "interactive login", meaning it is called both on normal
    login, and on "remember me" login.

--- app/src/EventSubscriber/LocaleSubscriber.php
+++ app/src/EventSubscriber/LocaleSubscriber.php
@@ -2,39 +2,35 @@
 
 namespace App\EventSubscriber;
 
+use App\Service\LocaleService;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class LocaleSubscriber implements EventSubscriberInterface
 {
-    private string $defaultLocale;
-
-    public function __construct(string $defaultLocale = 'fr')
-    {
-        $this->defaultLocale = $defaultLocale;
+    public function __construct(
+        private LocaleService $localeService,
+    ) {
     }
 
     public function onKernelRequest(RequestEvent $event): void
     {
         $request = $event->getRequest();
-        if (!$request->hasPreviousSession()) {
-            return;
+
+        $supportedLocalesCodes = $this->localeService->getSupportedLocalesCodes();
+        $locale = $request->getPreferredLanguage($supportedLocalesCodes);
+
+        if ($request->hasPreviousSession()) {
+            $locale = $request->getSession()->get('_locale', $locale);
         }
 
-        // try to see if the locale has been set as a _locale routing parameter
-        if ($locale = $request->attributes->get('_locale')) {
-            $request->getSession()->set('_locale', $locale);
-        } else {
-            // if no explicit locale has been set on this request, use one from the session
-            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
-        }
+        $request->setLocale($locale);
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            // must be registered before (i.e. with a higher priority than) the default Locale listener
             KernelEvents::REQUEST => [['onKernelRequest', 20]],
         ];
     }
