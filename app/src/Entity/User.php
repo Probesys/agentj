12f547bd4e332c54383732e606c8df2a129faffd
Christian TRESVAUX <christian.tresvaux@probesys.com>
2026-05-18 09:39:51 +0200

    fix: return email if user fullname is null

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -206,9 +206,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getFullname(): ?string
+    public function getFullname(): string
     {
-        return $this->fullname;
+        $fullname = $this->fullname;
+        if ($fullname === null || trim($fullname) === '') {
+            return $this->getEmail();
+        }
+
+        return $fullname;
     }
 
     public function setFullname(?string $fullname): self
