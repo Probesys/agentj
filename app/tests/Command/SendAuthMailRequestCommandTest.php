5c9991e2b13fe98638caece9f898a29590ebe8aa
Guillaume DUPRAZ-CANARD <guillaume.dupraz-canard@probesys.coop>
2026-08-31 17:55:01 +0200

    wip: add test on several email sending

--- app/tests/Command/SendAuthMailRequestCommandTest.php
+++ app/tests/Command/SendAuthMailRequestCommandTest.php
@@ -3,6 +3,8 @@
 namespace App\Tests\Command;
 
 use App\Amavis\MessageStatus;
+use App\Entity\Message;
+use App\Tests\Factory\AddressFactory;
 use App\Tests\Factory\DomainFactory;
 use App\Tests\Factory\MessageFactory;
 use App\Tests\Factory\UserFactory;
@@ -25,16 +27,24 @@ class SendAuthMailRequestCommandTest extends KernelTestCase
     use ResetDatabase;
     use SessionHelper;
 
+    private EntityManagerInterface $entityManager;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->entityManager = self::getContainer()->get(EntityManagerInterface::class);
+    }
+
     public function testAuthMailIsSentToNotAuthenticatedSender(): void
     {
         $domain = DomainFactory::createOne();
         $recipient = UserFactory::new()->user($domain)->create();
         $sender = UserFactory::new()->user($domain)->create();
         [$addrS, $addrR] = $this->setupAddresses($sender, $recipient);
-        $mailId = $this->setupMail($addrS, $addrR, status: MessageStatus::UNRELEASED);
+        $message = $this->setupMail($addrS, $addrR, status: MessageStatus::UNRELEASED);
         // Generate a mail sent 6 hours ago, and whom the sender is unauthenticated
         $em = self::getContainer()->get(EntityManagerInterface::class);
-        $message = MessageFactory::findBy(['mailId' => $mailId])[0];
+        $message = MessageFactory::findBy(['messaged' => $message])[0];
         $message->setSendCaptcha(0);
         $message->setTimeNum(new DateTimeImmutable("-6 hours")->getTimestamp());
         $messageRecipient = $message->getMessageRecipients()->first();
@@ -61,16 +71,48 @@ class SendAuthMailRequestCommandTest extends KernelTestCase
         ]);
     }
 
+    public function testAuthMailIsSentToNotAuthenticatedSenderSeveralTimes(): void
+    {
+        $domain = DomainFactory::createOne();
+        $recipient = UserFactory::new()->user($domain)->create();
+        $sender = UserFactory::new()->user($domain)->create();
+        [$addrS, $addrR] = $this->setupAddresses($sender, $recipient);
+        // Generate a mail sent 8 hours ago, and whom the sender is unauthenticated
+        $message = $this->setupMail($addrS, $addrR, status: MessageStatus::UNRELEASED);
+        $this->setMessageDate($message, '-8 hours');
+        // Generate a mail sent 6 hours ago, and whom the sender is unauthenticated
+        $senderAddressOtherCase = AddressFactory::createOne([
+            'domain' => $addrS->getDomain(),
+            'partitionTag' => 0,
+            'email' => strtoupper($sender->getEmail()),
+        ]);
+        $message = $this->setupMail($senderAddressOtherCase, $addrR, status: MessageStatus::UNRELEASED);
+        $this->setMessageDate($message, '-6 hours');
+        self::bootKernel();
+        // Create a mock to intercept mails and assert 2 are sent to the sender
+        $mailer = $this->createMock(MailerInterface::class);
+        $mailer->expects($this->exactly(2))
+            ->method('send');
+        self::getContainer()->set(MailerInterface::class, $mailer);
+
+        $application = new Application(self::$kernel);
+        $command = $application->find('agentj:send-auth-mail-token');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+        ]);
+    }
+
     public function testAuthMailIsNotSentIfSenderIsAMailingList(): void
     {
         $domain = DomainFactory::createOne();
         $recipient = UserFactory::new()->user($domain)->create();
         $sender = UserFactory::new()->user($domain)->create();
         [$addrS, $addrR] = $this->setupAddresses($sender, $recipient);
-        $mailId = $this->setupMail($addrS, $addrR, status: MessageStatus::UNRELEASED);
+        $message = $this->setupMail($addrS, $addrR, status: MessageStatus::UNRELEASED);
         // Generate a mail sent 6 hours ago, and whom the sender is a mailing list (unauthenticated)
         $em = self::getContainer()->get(EntityManagerInterface::class);
-        $message = MessageFactory::findBy(['mailId' => $mailId])[0];
+        $message = MessageFactory::findBy(['messaged' => $message])[0];
         $message->setSendCaptcha(0);
         $message->setTimeNum(new DateTimeImmutable("-6 hours")->getTimestamp());
         $message->setIsMlist(true);
@@ -99,10 +141,10 @@ class SendAuthMailRequestCommandTest extends KernelTestCase
         $recipient = UserFactory::new()->user($domain)->create();
         $sender = UserFactory::new()->user($domain)->create();
         [$addrS, $addrR] = $this->setupAddresses($sender, $recipient);
-        $mailId = $this->setupMail($addrS, $addrR);
+        $message = $this->setupMail($addrS, $addrR);
         // Generate a mail sent 6 hours ago, but not yet processed by Amavis
         $em = self::getContainer()->get(EntityManagerInterface::class);
-        $message = MessageFactory::findBy(['mailId' => $mailId])[0];
+        $message = MessageFactory::findBy(['messaged' => $message])[0];
         $message->setSendCaptcha(0);
         $message->setTimeNum(new DateTimeImmutable("-6 hours")->getTimestamp());
         $em->flush();
@@ -127,10 +169,10 @@ class SendAuthMailRequestCommandTest extends KernelTestCase
         $recipient = UserFactory::new()->user($domain)->create();
         $sender = UserFactory::new()->user($domain)->create();
         [$addrS, $addrR] = $this->setupAddresses($sender, $recipient);
-        $mailId = $this->setupMail($addrS, $addrR);
+        $message = $this->setupMail($addrS, $addrR);
         // Generate a mail sent 6 hours ago
         $em = self::getContainer()->get(EntityManagerInterface::class);
-        $message = MessageFactory::findBy(['mailId' => $mailId])[0];
+        $message = MessageFactory::findBy(['messaged' => $message])[0];
         $message->setSendCaptcha(0);
         $message->setTimeNum(new DateTimeImmutable("-6 hours")->getTimestamp());
         $messageRecipient = $message->getMessageRecipients()->first();
@@ -158,17 +200,17 @@ class SendAuthMailRequestCommandTest extends KernelTestCase
         $recipient = UserFactory::new()->user($domain)->create();
         $sender = UserFactory::new()->user($domain)->create();
         [$addrS, $addrR] = $this->setupAddresses($sender, $recipient);
-        $mailId = $this->setupMail($addrS, $addrR);
+        $message = $this->setupMail($addrS, $addrR);
         $otherMailId = $this->setupMail($addrS, $addrR, 'otherTest');
         $em = self::getContainer()->get(EntityManagerInterface::class);
-        $message = MessageFactory::findBy(['mailId' => $mailId])[0];
+        $message = MessageFactory::findBy(['messaged' => $message])[0];
         $message->setSendCaptcha(0);
         $message->setTimeNum(new DateTimeImmutable("-6 hours")->getTimestamp());
         $messageRecipient = $message->getMessageRecipients()->first();
         self::assertNotFalse($messageRecipient);
         $messageRecipient->setStatus(MessageStatus::UNTREATED);
         // Create another message sent 6 hours ago and for which authentication mail has already been sent
-        $otherMessage = MessageFactory::findBy(['mailId' => $otherMailId])[0];
+        $otherMessage = MessageFactory::findBy(['messaged' => $otherMailId])[0];
         $otherMessage->setSendCaptcha(new DateTimeImmutable("-6 hours")->getTimestamp());
         $otherMessage->setTimeNum(new DateTimeImmutable("-6 hours")->getTimestamp());
         $otherMessageRecipient = $message->getMessageRecipients()->first();
@@ -188,5 +230,15 @@ class SendAuthMailRequestCommandTest extends KernelTestCase
         $commandTester->execute([
             'command' => $command->getName(),
         ]);
+    }
+
+    private function setMessageDate(Message $message, ?string $delta = '-6 hours'): void
+    {
+        $message->setSendCaptcha(0);
+        $message->setTimeNum(new DateTimeImmutable($delta)->getTimestamp());
+        $messageRecipient = $message->getMessageRecipients()->first();
+        self::assertNotFalse($messageRecipient);
+        $messageRecipient->setStatus(MessageStatus::UNTREATED);
+        $this->entityManager->flush();
     }
 }
