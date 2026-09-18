5aad4a253e3f47f96081346a3d5f0f01b9fc58de
Guillaume DUPRAZ-CANARD <guillaume.dupraz-canard@probesys.coop>
2026-09-18 14:59:04 +0200

    chore: add tests to cover the whole command code

--- app/tests/Command/AmavisAutoReleaseCommandTest.php
+++ app/tests/Command/AmavisAutoReleaseCommandTest.php
@@ -3,6 +3,12 @@
 namespace App\Tests\Command;
 
 use App\Amavis\MessageStatus;
+use App\Command\AmavisAutoReleaseCommand;
+use App\Repository\MessageRecipientRepository;
+use App\Repository\MessageRecipientSearchRepository;
+use App\Repository\SenderRuleRepository;
+use App\Repository\UserRepository;
+use App\Service\MessageService;
 use App\Tests\Factory\DomainFactory;
 use App\Tests\Factory\RuleAddressFactory;
 use App\Tests\Factory\SenderRuleFactory;
@@ -12,7 +18,10 @@ use App\Tests\MessageHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\SharedLockInterface;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
@@ -29,6 +38,87 @@ class AmavisAutoReleaseCommandTest extends KernelTestCase
     {
         parent::setUp();
         $this->entityManager = self::getContainer()->get(EntityManagerInterface::class);
+    }
+
+    public function testAmavisCheckParentAliasUserToReleaseMail(): void
+    {
+        $domain = DomainFactory::createOne();
+        $otherDomain = DomainFactory::createOne();
+        $recipient = UserFactory::new()->user($domain)->create();
+        $recipient->setHumanAuthenticationEnabled(false);
+        $sender = UserFactory::new()->user($otherDomain)->create();
+        $alias = UserFactory::new()->alias($recipient)->create([
+            'email' => 'alias@' . $otherDomain,
+        ]);
+        [$addrS, $addrR] = $this->setupAddresses($sender, $alias);
+        $message = $this->setupMail($addrS, $addrR, status: MessageStatus::UNRELEASED);
+
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+        $command = $application->find('agentj:auto-release-message');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+        ]);
+
+        $messageRecipient = $message->getMessageRecipients()->first();
+        self::assertNotFalse($messageRecipient);
+        $this->refresh($messageRecipient);
+        self::assertSame(MessageStatus::RESTORED, $messageRecipient->getStatus());
+    }
+
+    public function testAmavisRestoreMailIfHumanAuthenticationDisabledAndNotDetectedAsSpam(): void
+    {
+        $domain = DomainFactory::createOne();
+        $otherDomain = DomainFactory::createOne();
+        $recipient = UserFactory::new()->user($domain)->create();
+        $recipient->setHumanAuthenticationEnabled(false);
+        $sender = UserFactory::new()->user($otherDomain)->create();
+        [$addrS, $addrR] = $this->setupAddresses($sender, $recipient);
+        $message = $this->setupMail($addrS, $addrR, status: MessageStatus::UNRELEASED);
+
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+        $command = $application->find('agentj:auto-release-message');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+        ]);
+
+        $messageRecipient = $message->getMessageRecipients()->first();
+        self::assertNotFalse($messageRecipient);
+        $this->refresh($messageRecipient);
+        self::assertSame(MessageStatus::RESTORED, $messageRecipient->getStatus());
+    }
+
+    public function testAmavisPassMailInSpamIfSpamDetected(): void
+    {
+        $domain = DomainFactory::createOne([
+            'level' => 0,
+        ]);
+        $otherDomain = DomainFactory::createOne();
+        $recipient = UserFactory::new()->user($domain)->create();
+        $sender = UserFactory::new()->user($otherDomain)->create();
+        [$addrS, $addrR] = $this->setupAddresses($sender, $recipient);
+        $message = $this->setupMail($addrS, $addrR, status: MessageStatus::UNRELEASED);
+        $messageRecipient = $message->getMessageRecipients()->first();
+        self::assertNotFalse($messageRecipient);
+        $messageRecipient->setBspamLevel(100);
+        $this->entityManager->persist($messageRecipient);
+        $this->entityManager->flush();
+
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+        $command = $application->find('agentj:auto-release-message');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+        ]);
+
+        $messageRecipient = $message->getMessageRecipients()->first();
+        self::assertNotFalse($messageRecipient);
+        $this->refresh($messageRecipient);
+        self::assertSame(MessageStatus::SPAMMED, $messageRecipient->getStatus());
     }
 
     public function testAmavisReleaseMailUsingSenderRuleAddress(): void
@@ -140,5 +230,39 @@ class AmavisAutoReleaseCommandTest extends KernelTestCase
         self::assertNotFalse($messageRecipient);
         $this->refresh($messageRecipient);
         self::assertSame(MessageStatus::UNTREATED, $messageRecipient->getStatus());
+    }
+
+    public function testExecuteFailsWhenAutoReleaseLockCannotBeAcquired(): void
+    {
+        $lock = $this->createMock(SharedLockInterface::class);
+        $lock
+            ->expects(self::once())
+            ->method('acquire')
+            ->willReturn(false);
+
+        $lockFactory = $this->createMock(LockFactory::class);
+        // Take a lock, to prevent the command to acquire one
+        $lockFactory
+            ->expects(self::once())
+            ->method('createLock')
+            ->with('msgs-auto-release', 1800)
+            ->willReturn($lock);
+
+        $command = new AmavisAutoReleaseCommand(
+            $this->createMock(MessageRecipientRepository::class),
+            $this->createMock(MessageRecipientSearchRepository::class),
+            $this->createMock(UserRepository::class),
+            $this->createMock(SenderRuleRepository::class),
+            $this->createMock(MessageService::class),
+            $lockFactory,
+        );
+        $commandTester = new CommandTester($command);
+        $exitCode = $commandTester->execute([]);
+
+        self::assertSame(Command::FAILURE, $exitCode);
+        self::assertStringContainsString(
+            "Can't acquire the msgs-auto-release lock, the command is probably already running.",
+            $commandTester->getDisplay(),
+        );
     }
 }
